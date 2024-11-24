6164b3aaa9d6ae134dc383d9e82b202fe513d056
Provab Mongchaw Khyang <provabben@gmail.com>
2024-11-24 15:43:59 +0600

    Added Easy Project num_1

---
+++ Projects/1. Easy/1.rockPaperScissors.cpp
@@ -0,0 +1,6 @@
+// ? Create a function which simulates the game "rock, paper, scissors". The function takes the input of one player (rock, paper or scissors), the only parametre that will be sent
+// * The Code will Randomly CHoose either rock paper or scissor
+// * Based on the out come the output screen will either shoe Player Won or Player Lost
+// !
+
+// TODO: Start Your Solve Here
